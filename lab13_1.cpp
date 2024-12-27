e6eaebbbbef17777c2de4f357262926766b11e31
9nattch <nattchayadao@gmail.com>
2024-12-28 00:53:45 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,36 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+
+void stat(const double A[], int N, double B[]){
+    double sum = 0.0 , variance = 0.0, geomean = 1.0, harmo = 0;
+    double mean, std_deviation, Geometric_mean, Harmonic_mean, Max = A[0], Min = A[0];
+    
+for(int i = 0; i < N; i++){
+    sum += A[i];
+ }
+    mean = sum/N;
+
+for (int i = 0; i < N; i++){
+    variance += pow((A[i] - mean),2);
+    std_deviation = sqrt(variance / N);
+    geomean *= A[i];
+    Geometric_mean = pow(geomean, 1.0/N);
+    harmo += 1.0 / A[i];
+    Harmonic_mean = N / harmo;
+
+}
+for(int i = 1; i < N; i++){
+    if (A[i] > Max){
+        Max = A[i];
+    }if (A[i] < Min){
+    Min = A[i];
+}
+}
+B[0] = mean;
+B[1] = std_deviation;
+B[2] = Geometric_mean;
+B[3] = Harmonic_mean;
+B[4] = Max;
+B[5] = Min;
+}
